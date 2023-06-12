a8cbcb9f51d1a978e99c82f60b5e04128086f286
yixiaowang2001 <ywang8@macalester.edu>
2023-06-12 12:37:49 +0800

    Solve 429 error

--- amazon_scraper/amazon_scraper/settings.py
+++ amazon_scraper/amazon_scraper/settings.py
@@ -8,6 +8,7 @@
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from amazon_scraper.config import API_KEY
+import random
 
 BOT_NAME = "amazon_scraper"
 
@@ -29,12 +30,12 @@ DOWNLOADER_MIDDLEWARES = {
 }
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = random.uniform(0.5, 1.5)
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
